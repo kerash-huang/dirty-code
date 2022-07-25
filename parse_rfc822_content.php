050bba865b5d6f46905b83f89076ac65d4bfe8d1
Kerash <blog@kerash.tw>
2022-07-25 16:22:24 +0800

    Update parse_rfc822_content.php
    add image

--- parse_rfc822_content.php
+++ parse_rfc822_content.php
@@ -6,6 +6,10 @@ put the plain text string and will return an array
 
 main data index will named  content-body-{CONTENT-TYPE}
 ex: contet-body-text-plain  or content-body-text-html
+
+add image index into [image-attachment] array
+with filename as sub-index name
+['image-attachment']['image.png']
 */
 function parse_rfc822_content($content) {
     $content_line = explode("\r\n", $content); // 切換行
@@ -15,27 +19,47 @@ function parse_rfc822_content($content) {
     $is_in_content = false; // 是否已經進入內容
     $is_start_store_content = false; // 是否開始儲存內容
     $in_content_text  = ''; // 內容資料
-    $is_new_dataset = false;
-    $is_base64_type = false;
-    foreach ($content_line as $_line) {
-        if ($is_in_content or strpos($_line, '--') === 0) { // 實際的內容
+    $is_new_dataset = false; // 新一個資料
+    $is_base64_type = false; // 是不是b64的內容
+    $tmp_image_name = ''; // 暫存圖片名稱
+    foreach ($content_line as $_line) { // 一行一行執行
+        if ($is_in_content or strpos($_line, '--') === 0) { // 已經在存實際的內容(-- 開頭或者標定已在儲存)
             $is_new_dataset = false;
-            if (strrpos($_line, '--') !== 0 and strrpos($_line, '--') !== false) { // 結尾
-                $content_data[$line_name] = $in_content_text;
+            if (strrpos($_line, '--') !== 0 and strrpos($_line, '--') !== false) { // end of content set
+
+                if ($line_name and $in_content_text) {
+                    if (!$tmp_image_name and $is_base64_type) {
+                        $in_content_text = trim($in_content_text, "\r\n\0");
+                        $in_content_text = base64_decode($in_content_text);
+                    }
+                    if ($tmp_image_name) {
+                        $content_data[$line_name][$tmp_image_name] = $in_content_text;
+                    } else {
+                        $content_data[$line_name] = $in_content_text;
+                    }
+                }
+
                 $is_in_content = false;
                 $is_start_store_content = false;
+                $is_base64_type = false;
                 $in_content_text = '';
                 $line_name = '';
+                $tmp_image_name = '';
             } else {
                 if (strpos($_line, '--') === 0) {
                     $is_in_content = true;
                     if ($line_name and $in_content_text) {
-                        if ($is_base64_type) {
+                        if (!$tmp_image_name and $is_base64_type) {
                             $in_content_text = trim($in_content_text, "\r\n\0");
                             $in_content_text = base64_decode($in_content_text);
                         }
-                        $content_data[$line_name] = $in_content_text;
+                        if ($tmp_image_name) {
+                            $content_data[$line_name][$tmp_image_name] = $in_content_text;
+                        } else {
+                            $content_data[$line_name] = $in_content_text;
+                        }
                     }
+                    $tmp_image_name = '';
                     $is_start_store_content = false;
                     $is_base64_type = false;
                     $in_content_text = '';
@@ -50,22 +74,38 @@ function parse_rfc822_content($content) {
                                 $split_2 = explode(': ', $split_1[0]);
                                 $name_ext = str_replace('/', '-', $split_2[1]);
                                 $line_name .= '-' . $name_ext;
+
+                                if (strpos($name_ext, 'image') !== false) {
+                                    $line_name = 'image-attachment';
+                                    // 圖
+                                    $split_2 = explode("=", $split_1[1]);
+                                    $img_file_name = $split_2[1];
+                                    $img_file_name = str_replace('"', '', $img_file_name);
+                                    $tmp_image_name = $img_file_name;
+                                }
                             } else if (strpos(strtolower($_line), 'content-transfer-encoding') !== false) {
-                                if (strpos(strtolower($_line), 'base64') !== false) {
+                                if (!$tmp_image_name and strpos(strtolower($_line), 'base64') !== false) {
                                     $is_base64_type = true;
                                 }
                             }
                         }
                     } else {
-                        $in_content_text .= $_line . "\r\n";
+                        $in_content_text .= $_line;
+                        if (!$tmp_image_name) {
+                            $in_content_text .= "\r\n";
+                        }
                     }
                 }
             }
-        } else if (substr($_line, 0, 1) == ' ') { // 與此前同一份資料, line name 不換，直接串上內容
+        } else if (substr($_line, 0, 1) == ' ') { // for header 的，與此前同一份資料, line name 不換，直接串上內容
             if ($is_new_dataset) {
-                $content_data[$line_name] .= ltrim("\r\n" . $_line, ' ');
+                if ($tmp_image_name) {
+                    $content_data[$line_name][$tmp_image_name] .= ltrim("\r\n" . $_line, ' ');
+                } else {
+                    $content_data[$line_name] .= ltrim("\r\n" . $_line, ' ');
+                }
             }
-        } else {
+        } else { // for header 撈 source mail , topic ... etc 資料用
             if (empty($_line)) { // 空的直接跳過
                 continue;
             }
@@ -77,8 +117,8 @@ function parse_rfc822_content($content) {
             }
 
             $line_name = strtolower($_dline_box[0]);
-            $is_new_dataset = true;
-            $content_data[$line_name] = $_dline_box[1];
+            $is_new_dataset = true;  // 新一行資料
+            $content_data[$line_name] = $_dline_box[1]; // 這時候通常不會有 image 所以不用管
         }
     }
     return $content_data;
